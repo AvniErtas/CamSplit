6094b27514607009db74acc4145e42163c346085
avnie <avniertas@outlook.com>
2020-07-24 00:01:54 +0300

    fastprogram

---
+++ android/app/src/main/kotlin/com/example/camsplit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.camsplit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'CameraPage.dart';
+
+void main() {
+  runApp(CamSplitMainPage());
+}
+
+class CamSplitMainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.black));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CameraPage(),
+    );
+  }
+}
